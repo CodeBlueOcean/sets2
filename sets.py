670936cac90a7ca1a2bbb6ffa687421f365f349b
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-03-07 12:55:19 -0600

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,47 @@
+# None
+# bool
+# int
+# float
+# # complex
+# str
+# list
+# tuple
+# dict
+
+# set the data structure and in a set there is no duplication
+# my set already contains 2
+
+
+# my_set = {1,2,3,4,5,5}
+# my_set.add(100)
+# my_set.add(2)
+# print(my_set)
+
+
+my_list = [1,2,3,4,5,5]
+print(set(my_list))
+
+#imagine we have emails or deplicates
+
+# another example shows one is true
+my_set = {1,2,3,4,5,5}
+print(1 in my_set)
+
+# another example
+my_set = {1,2,3,4,5,5}
+print(len(my_set))
+
+# another example
+my_set = {1,2,3,4,5,5}
+print(list(my_set))
+
+# another example
+my_set = {1,2,3,4,5,5}
+new_set = my_set.copy()
+my_set.clear()
+print(new_set)
+print(my_set)
+
+
+
+
---
+++ sets2.py
@@ -0,0 +1,23 @@
+# my_set = {1,2,3,4,5,}
+# your_set = {4,5,6,7,8,9,10}
+
+# print(my_set.difference(your_set))
+# print(my_set.discard(5))
+# print(my_set)
+# print(my_set.difference_update(your_set))
+# print(my_set)
+# print(my_set.intersection(your_set))
+# ## shorthand print print(my_self & your_set)
+# print(my_set.isdisjoin(your_set))
+# ## isdisjoin means both sets have nothing in common
+# print(my_set.union(your_set))
+# ## shorthand print(my_set | your_set)
+
+# another example
+my_set = {4,5}
+your_set = {4,5,6,7,8,9,10}
+print(my_set.issubset(your_set))
+print(your_set.issuperset(my_set))
+
+
+
